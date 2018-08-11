710e63a2af941265a8b6a4591af79f5dd076b023
George Georgiev <george.georgiev96@gmail.com>
2018-08-11 14:50:08 +0300

    Add base project react entry points

--- res/js/main.js
+++ res/js/main.js
@@ -1,1 +1,13 @@
-import "../sass/main.scss";
+/**
+ * The external dependencies.
+ */
+import '../sass/main.scss';
+import React from 'react';
+import { render } from 'react-dom';
+
+/**
+ * The internal dependencies.
+ */
+import App from './containers/App';
+
+render(<App />), document.getElementById('app');
